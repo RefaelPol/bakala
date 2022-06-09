6626a289e0036e3e8522fefcc06d37e7c0ed987c
Refael <polochin014@gmail.com>
2022-06-09 15:57:28 +0300

    scriiipt

--- app.js
+++ app.js
@@ -9,14 +9,64 @@ function addTax(price) {
 }
 
 function generateId() {
-    // const rand = Math.random();
-    // console.log(`rand: ${rand}`);
-    // const num = rand * 100000;
-    // console.log(`num: ${num}`);
-    // const floor = Math.floor(num);
-    // console.log(`floor: ${floor}`);
-    // return floor;
-
     const time = new Date().getTime();
     return Math.floor(Math.random() * time);
 }
+
+const products = [{
+        id: generateId(),
+        price: 15,
+        name: 'yellow cheese',
+        image: '2016/03/05/19/24/cheese-1238395_960_720.jpg',
+        description: 'great product really!',
+    },
+    {
+        id: generateId(),
+        price: 3,
+        name: 'ice cream',
+        image: '2017/04/18/15/10/strawberry-ice-cream-2239377_640.jpg',
+        description: 'be careful - very cold',
+    },
+];
+
+
+
+
+function sortProducts() {
+    const sortField = document.querySelector('#sort-field');
+    const sortDirection = sortField.value;
+
+    //sort array of products
+
+}
+
+
+function getProductsTemplate() {
+    const IMG_PATH = 'https://cdn.pixabay.com/photo/'; // configuration of the project
+    let html = '';
+
+    products.forEach(prod => {
+        html +=
+            `<div class="card" style="width: 18rem;">
+                <img src="${IMG_PATH}${prod.image}"
+                    class="card-img-top" alt="${prod.description}">
+                <div class="card-body">
+                    <h5 class="card-title">${prod.name}</h5>
+                    <p class="card-text">$
+                        ${addTax(prod.price)}</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+            </div>`;
+    })
+
+    return html;
+}
+
+
+function displayProduct() {
+    const container = document.getElementById('products-container');
+    const productHtml = getProductsTemplate();
+    container.innerHTML = productHtml;
+}
+
+displayProduct();
